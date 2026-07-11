0c7b0a78195870d9342d6aa43afbf533b6bf81c5
Poojana-S <poojanas.dev@gmail.com>
2026-07-11 09:27:00 +0530

    Route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,15 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.project_name}-igw"
   }
 }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}
