799482e7e5718c98070341b95e1a74526fc02693
Poojana-S <poojanas.dev@gmail.com>
2026-07-11 09:28:40 +0530

    AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,3 +98,18 @@ resource "aws_security_group" "app_sg" {
     Name = "${var.project_name}-app-sg"
   }
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
